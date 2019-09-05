062dc862d403fa51c1c51f68652e64411c8faa47
Liam Ozog <liam@ada.support>
2019-09-04 23:55:46 -0400

    add call to google sheets api to get budget data for widget

---
+++ app/src/main/java/com/example/expensetracker/SummaryWidget.kt
@@ -0,0 +1,148 @@
+package com.example.expensetracker
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
+import android.util.Log
+import android.widget.RemoteViews
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.util.*
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class SummaryWidget : AppWidgetProvider() {
+
+    private val LOG = "SUMMARY_WIDGET"
+    private val ACTION_UPDATE = "action.UPDATE"
+
+    private lateinit var results: Array<String>
+    private var numCols: Int = 0
+    private var numRows: Int = 0
+    private var categories = ArrayList<String>()
+    private var amounts = ArrayList<String>()
+
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            // Get the layout for the SummaryWidget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.summary_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.update_button, getPendingSelfIntent(context, ACTION_UPDATE));
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        Log.d(LOG, "onReceive " + intent.action!!)
+
+        if (ACTION_UPDATE == intent.action) {
+            callSheetsAPI(context)
+        }
+    }
+
+    private fun getPendingSelfIntent(context: Context, action: String): PendingIntent {
+        val intent =
+            Intent(context, javaClass) // An intent directed at the current class (the "self").
+        intent.action = action
+        return PendingIntent.getBroadcast(context, 0, intent, 0)
+    }
+
+    private fun callSheetsAPI(context: Context) {
+        // Instantiate the RequestQueue
+        val queue = Volley.newRequestQueue(context)
+
+        val url = context.getString(R.string.google_sheets_url)
+
+        // Request a string response from the provided URL
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener { response ->
+                results =
+                    response.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                numRows = results.size - 1 // TODO: I added a "total" count so -1 for that
+                numCols =
+                    results[0].split("\t".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
+                        .size
+
+                categories.clear()
+                amounts.clear()
+                val cal = Calendar.getInstance()
+                val monthCol = cal.get(Calendar.MONTH) + 2 // add offset of 2 (Jan = col 2)
+
+                for (row in 0 until numRows) {
+                    var col = 0 // category
+                    val rowArr = results[row].split("\t".toRegex()).dropLastWhile({ it.isEmpty() })
+                        .toTypedArray()
+                    categories.add(rowArr[col])
+                    col = monthCol
+                    amounts.add(rowArr[col])
+                    //                            Log.d(LOG, rowArr[col]);
+                }
+
+                updateUI(context)
+            }, Response.ErrorListener { error -> Log.e(LOG, error.toString()) })
+
+        // add the request to the RequestQueue
+        queue.add(stringRequest)
+    }
+
+    private fun updateUI(context: Context) {
+        val appWidgetManager = AppWidgetManager.getInstance(
+            context
+                .applicationContext
+        )
+
+        val thisWidget = ComponentName(
+            context,
+            SummaryWidget::class.java!!
+        )
+        val allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
+
+        allWidgetIds.forEach { widgetId ->
+            val remoteViews = RemoteViews(
+                context
+                    .applicationContext.packageName,
+                R.layout.summary_widget
+            )
+
+            // set the text
+            remoteViews.setTextViewText(
+                R.id.summary_text,
+                amounts.toString()
+            )
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews)
+        }
+    }
+}
+
