009d0b061de31168fe67ac904a19ea7bc4ce23af
Liam Ozog <liam@ada.support>
2021-10-22 13:59:14 -0400

    code cleanup

--- app/src/main/java/com/lozog/expensetracker/ui/SheetsViewModel.kt
+++ app/src/main/java/com/lozog/expensetracker/ui/SheetsViewModel.kt
@@ -15,51 +15,7 @@ import java.io.IOException
 class SheetsViewModel : ViewModel() {
     companion object {
         private const val TAG = "SHEETS_VIEW_MODEL"
-//
-//        private const val SHEETS_VALUE_INPUT_OPTION = "USER_ENTERED"
-//        private const val SHEETS_INSERT_DATA_OPTION = "INSERT_ROWS"
-//
-//        // January -> column C, etc
-//        // TODO: dynamically find month columns
-//        private val MONTH_COLUMNS = listOf(
-//            "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
-//        )
-//
-//        // TODO: dynamically find category cell
-//        private val CATEGORY_ROW_MAP = mapOf(
-//            "Groceries" to "20",
-//            "Dining Out" to "21",
-//            "Drinks" to "22",
-//            "Material Items" to "23",
-//            "Entertainment" to "24",
-//            "Transit" to "25",
-//            "Personal/Medical" to "26",
-//            "Gifts" to "27",
-//            "Travel" to "28",
-//            "Miscellaneous" to "29",
-//            "Film" to "30",
-//            "Household" to "31",
-//            "Other Income" to "5"
-//        )
-//        val CATEGORIES = arrayOf(
-//            "Groceries",
-//            "Dining Out",
-//            "Drinks",
-//            "Material Items",
-//            "Entertainment",
-//            "Transit",
-//            "Personal/Medical",
-//            "Gifts",
-//            "Travel",
-//            "Miscellaneous",
-//            "Film",
-//            "Household",
-//            "Other Income"
-//        )
     }
-
-//    private val job = Job()
-//    private val coroutineScope = CoroutineScope(Dispatchers.IO + job)
 
     private val sheetsRepository = SheetsRepository()
 
@@ -77,13 +33,6 @@ class SheetsViewModel : ViewModel() {
     fun resetView() {
         status.value = SheetsStatus.DONE
     }
-
-//    override fun onCleared() {
-//        super.onCleared()
-//        job.cancel()
-//    }
-
-//    /********** GOOGLE SHEETS METHODS **********/
 
     fun addExpenseRowToSheetAsync(
         spreadsheetId: String,
@@ -146,9 +95,6 @@ class SheetsViewModel : ViewModel() {
                 setStatusText(statusText)
                 setStatus(SheetsStatus.DONE)
             }
-
-//            setStatusText(statusText)
-//            submitButton.text = getString(R.string.button_expense_submit)
         }
     }
 }
