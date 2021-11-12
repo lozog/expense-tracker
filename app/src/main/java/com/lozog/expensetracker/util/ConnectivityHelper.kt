4854dbef700a1c77caf53f8af79566ec17246cba
Liam Ozog <liam@ada.support>
2021-11-12 13:28:58 -0500

    keep both versions of internet connectivity code, but use the deprecated one for now

--- app/src/main/java/com/lozog/expensetracker/util/ConnectivityHelper.kt
+++ app/src/main/java/com/lozog/expensetracker/util/ConnectivityHelper.kt
@@ -9,10 +9,19 @@ import android.util.Log
 class ConnectivityHelper {
     companion object {
         private const val TAG = "CONNECTIVITY_HELPER"
+        private const val SHOULD_CHECK_INTERNET_WITH_TRANSPORT = false
+
+        fun isInternetConnected(context: Context): Boolean {
+            return if (SHOULD_CHECK_INTERNET_WITH_TRANSPORT) {
+                isInternetConnectedWithTransport(context)
+            } else {
+                isInternetConnectedWithActiveNetworkInfo(context)
+            }
+        }
 
         // this version of the code uses deprecated classes
         // but it appears to be more reliable (e.g. in the case of a VPN)
-        fun isInternetConnected(context: Context): Boolean {
+        private fun isInternetConnectedWithActiveNetworkInfo(context: Context): Boolean {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
 
@@ -23,27 +32,27 @@ class ConnectivityHelper {
 
         // this version is more modern, but doesn't seem to work in all cases
         // e.g. when connected to a VPN, TRANSPORT_WIFI is not present
-//        fun isInternetConnected(context: Context): Boolean {
-//            val connectivityManager =
-//                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-//
-//            val networkCapabilities = connectivityManager.activeNetwork ?: return false
-//            val actNw =
-//                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
-//
-//            Log.d(TAG, "capabilities: $actNw")
-//            Log.d(TAG, "wifi ${$actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)}")
-//            Log.d(TAG, "cellular ${$actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)}")
-//            Log.d(TAG, "ethernet ${$actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)}")
-//
-//            val result = when {
-//                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-//                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-//                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-//                else -> false
-//            }
-//
-//            return result
-//        }
+        private fun isInternetConnectedWithTransport(context: Context): Boolean {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+
+            Log.d(TAG, "capabilities: $actNw")
+            Log.d(TAG, "wifi ${actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)}")
+            Log.d(TAG, "cellular ${actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)}")
+            Log.d(TAG, "ethernet ${actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)}")
+
+            val result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+
+            return result
+        }
     }
 }
