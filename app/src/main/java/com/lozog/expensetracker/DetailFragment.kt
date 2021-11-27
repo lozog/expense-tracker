f6e99b483e99b1490b89d27ae47e2bc2736e3a46
Liam Ozog <liam@ada.support>
2021-11-27 18:21:33 -0500

    use fragment_form.xml as layout for DetailFragment

--- app/src/main/java/com/lozog/expensetracker/DetailFragment.kt
+++ app/src/main/java/com/lozog/expensetracker/DetailFragment.kt
@@ -7,22 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.lozog.expensetracker.databinding.FragmentDetailBinding
-import com.lozog.expensetracker.util.HistoryAdapter
+import com.lozog.expensetracker.databinding.FragmentFormBinding
 import com.lozog.expensetracker.util.expenserow.ExpenseRow
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "row"
+private const val ROW_PARAM = "row"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
     private var row: Int = 0
-    private var _binding: FragmentDetailBinding? = null
+    private var _binding: FragmentFormBinding? = null
     private val binding get() = _binding!!
     private val sheetsViewModel: SheetsViewModel by viewModels {
         SheetsViewModelFactory((context?.applicationContext as ExpenseTrackerApplication).sheetsRepository)
@@ -36,19 +28,17 @@ class DetailFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            row = it.getInt(ARG_PARAM1)
+            row = it.getInt(ROW_PARAM)
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentFormBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val detailText = binding.detailText
-//        Log.d(TAG, "$expenseRow")
+        val detailText = binding.statusText
 
         sheetsViewModel.getExpenseRowByRow(row)
 
@@ -57,27 +47,6 @@ class DetailFragment : Fragment() {
             detailText.text = expenseRow.toString()
         })
 
-
-//        return inflater.inflate(R.layout.fragment_detail, container, false)
         return root
     }
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment DetailFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            DetailFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                }
-//            }
-//    }
 }
