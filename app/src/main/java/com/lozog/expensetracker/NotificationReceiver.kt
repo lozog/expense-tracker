475c2c6dc48d70a113bf63b4ffe5e2ec7a677d98
Liam Ozog <liam.ozog@gmail.com>
2024-11-07 00:39:56 -0500

    create Expense when notification intent received

--- app/src/main/java/com/lozog/expensetracker/NotificationReceiver.kt
+++ app/src/main/java/com/lozog/expensetracker/NotificationReceiver.kt
@@ -4,13 +4,38 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.lozog.expensetracker.util.expenserow.ExpenseRow
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class NotificationReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context?, intent: Intent?) {
+        val sheetsRepository = (context?.applicationContext as ExpenseTrackerApplication).sheetsRepository
+
         // Retrieve the notification text from the intent extras
-        val notificationText = intent?.getStringExtra("message")
+        val notificationText = intent?.getStringExtra("message") ?: ""
 
         // Perform the desired action with the notification text
         Log.d("EXPENSE_TRACKER NotificationReceiver", "Received: $notificationText")
+
+        val regex = "(\\d){1,7}\\.\\d\\d".toRegex() // matches 12.00, 120.00, 1200.00, etc
+        val matchResult = regex.find(notificationText) ?: return
+        val match = matchResult.value
+
+        val expenseRow = ExpenseRow(
+            SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date()),
+            "TODO",
+            "Miscellaneous",
+            match,
+            "",
+            "",
+            "",
+            "",
+            "",
+            ExpenseRow.STATUS_PENDING
+        )
+        sheetsRepository.addExpenseRowAsync(expenseRow)
     }
 }
